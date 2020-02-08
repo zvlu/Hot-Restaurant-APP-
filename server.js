1b10d28a37b5429b8732a4307ea9ef18f155f731
AndrewL <lawna12@gmail.com>
2020-02-08 11:24:01 -0500

    app.get

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const express = require('express')
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.get("/", function(req, res){
+    res.sendfile(path.join(__dirname, "index.html"))
+})
+
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
