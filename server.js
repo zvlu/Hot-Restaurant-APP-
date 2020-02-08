0da63ae24e65b733750e0f9739850dfbed75bbf0
AndrewL <lawna12@gmail.com>
2020-02-08 11:10:03 -0500

    server.js ports and app listening

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+})
